97bcb73ba181a531c82d464c7858a9f17ea1bd46
Jonathan Way <jonathan.way@loop.colum.edu>
2018-10-23 14:22:06 -0500

    Movement and Detection
    - added a script to the player character
    - created code to allow the character to move
    - adjusted the code so that it would switch animations accordingly
    - created a system for the character to detect whether they are on the ground or not

---
+++ Assets/Scripts/RobotControllerScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotControllerScript : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpeed = 10f;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private Animator anim;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+
+    private bool facingRight = true;
+    private bool grounded = false;
+    private float groundRadius = 0.2f;
+
+    void Start () {
+
+	}
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Update()
+    {
+        UpdateIsGrounded();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    private void UpdateIsGrounded()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+    }
+    private void Jump()
+    {
+
+    }
+}
