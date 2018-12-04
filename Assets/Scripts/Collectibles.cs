656ea48dd209173b93ac25e46b6c44390122a4ba
Jonathan Way <jonathan.way@loop.colum.edu>
2018-12-04 13:44:13 -0600

    re-adding scripts for code review

---
+++ Assets/Scripts/Collectibles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour {
+
+    //variable that needs to be set inside of Unity
+    [SerializeField]
+    private Animator anim;
+
+    //variables that dont need to be set in unity
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    //a coin count for how many chests the player has collected throughout the game
+    private static int coinCount;
+
+
+
+    //starts as soon as the game launches, gives the code access to the audio source, sprite renderer, and box collider
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    //once the player touchers the collectible then the collectible is destroyed and the player score is increased
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //plays the animation for the collectible, turns off its hitbox, plays its sound, and increases player score
+            anim.SetBool("Collected", true);
+            boxCollider2D.enabled = false;
+            coinCount++;
+            Debug.Log("Coin count: " + coinCount);
+            audioSource.Play();
+
+            //destorys the game object after everything is done
+            Destroy(gameObject, audioSource.clip.length);
+        }	
+	}
+}
